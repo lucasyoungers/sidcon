cf5533886c1cdbbd4d9b1cc35f5e7a5730402ecc
Lucas Youngers <lucasyoungers@Lucass-MacBook-Pro.local>
2026-01-17 10:50:54 -0800

    adds factory run functions

--- sidcon/faction.go
+++ sidcon/faction.go
@@ -14,3 +14,27 @@ type FactionProperties struct {
 	colonySupport int
 	bidTiebreaker int
 }
+
+func (p FactionProperties) RunFactory(factory Factory) error {
+	err := p.cubes.RemoveCubes(factory.Input())
+	if err != nil {
+		return err
+	}
+	p.cubes.AddCubes(factory.Output())
+	return nil
+}
+
+func (p FactionProperties) RunFactories(factories []Factory) error {
+	var inputs Cubes
+	var outputs Cubes
+	for _, factory := range factories {
+		inputs.AddCubes(factory.Input())
+		outputs.AddCubes(factory.Output())
+	}
+	err := p.cubes.RemoveCubes(inputs)
+	if err != nil {
+		return err
+	}
+	p.cubes.AddCubes(outputs)
+	return nil
+}
